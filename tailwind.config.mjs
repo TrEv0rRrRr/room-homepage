228e53ce066740e5fef6148d3684b2faf2c14db2
Trevor <valentinosolischang@gmail.com>
2025-01-11 00:47:56 -0500

    Add Tailwind CSS configuration, new SVG icons, and update layout styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "Dark-Gray": "hsl(0, 0%, 63%)",
+        "Very-Dark-Gray": "hsl(0, 0%, 27%)",
+      },
+      fontWeight: {
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+      fontFamily: {
+        LeagueSpartan: ['"League Spartan"'],
+      },
+      screens: {
+        sm: "360px",
+        tablet800: "884px",
+      },
+    },
+  },
+  plugins: [],
+};
